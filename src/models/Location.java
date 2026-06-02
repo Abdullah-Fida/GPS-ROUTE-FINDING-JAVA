0d70cd694088b9d716559be0f6b2d7d8f0504edb
Abdullah <abdullahfida645@gmail.com>
2026-06-02 12:58:30 +0500

    Location and Road Class maded

--- src/models/Location.java
+++ src/models/Location.java
@@ -1,4 +1,24 @@
 package models;
 
 public class Location {
+    private String locationName;
+    private int id;
+
+    Location(String locationName , int id){
+        this.locationName = locationName;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    @Override
+    public String toString(){
+        return "LocationName : " + locationName + " ID :" + id;
+    }
 }
--- src/models/Road.java
+++ src/models/Road.java
@@ -1,4 +1,31 @@
 package models;
 
+import javax.print.attribute.standard.Destination;
+
 public class Road {
+    Location socurceCity;
+    Location destinationCity;
+    double distance;
+
+    Road(Location destinationCity , double distance , Location socurceCity){
+        this.destinationCity = destinationCity;
+        this.distance = distance;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Location getDestinationCity() {
+        return destinationCity;
+    }
+
+    public Location getSocurceCity() {
+        return socurceCity;
+    }
+
+    @Override
+    public String  toString(){
+        return"Soucrce City : " + socurceCity+  " " +"Distance KM : " + distance +" "+ "DestinationCity : " + destinationCity  ;
+    }
 }
